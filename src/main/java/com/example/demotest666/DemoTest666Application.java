71012ca5336dec0329bb9fa9d6d2660f33660415
gz <979951807@qq.com>
2024-10-29 16:56:02 +0800

    one

---
+++ src/main/java/com/example/demotest666/DemoTest666Application.java
@@ -0,0 +1,13 @@
+package com.example.demotest666;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTest666Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTest666Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demotest666/DemoTest666ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotest666;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTest666ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
